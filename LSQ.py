ce2dab9a6e72f2369e6dde91512247402bc61a11
Canmanie <canmanie@iMac-werk.local>
2018-06-15 17:05:49 +0200

    LSQ but still won't run

--- LSQ.py
+++ LSQ.py
@@ -1,6 +1,6 @@
 #----Policy Evaluation (LSQ) see: https://www.cs.rutgers.edu/~mlittman/courses/robots03/lspi.pdf
 # input: samples, policy, new_policy
-# output: w (weights), A, b (matrices) (of policy)
+# output: w (weights) (of policy)
 # Definition: Use samples to approximate Q-values of old policy and new_policy
 # ???approximate phi, R, P (R is reward function, P is transition function)???
 #
@@ -17,3 +17,32 @@
 # end loop
 #
 # Solve linear system Aw = b to find w (may need SVD to invert A)
+
+import policy
+import numpy as np
+
+def LSQ(samples, pi, new_pi):
+	k = 1 #should initialize to k x k = size of basis
+	A = np.zeros(k,k)
+	b = np.zeros(k,1)
+	
+	for state,action,reward,next_state in samples:
+		phi = basis(state,action)
+		next_action = Policy.act(pi,state)
+		next_phi = basis(next_state,next_action)
+		
+		A = A + phi*(phi-next_phi*new_pi.discount)
+		b = b + phi*reward
+		
+	w = np.linalg.solve(a,b)
+
+#-----Compute basis
+# input: sampled (state, action) pair
+# output: phi (column vector of length equal to total number of basis functions, each entry is corresponding basis function evaluated at the (s,a) pair)
+# Definition: Computes the basis functions for given (state,action) pair
+#
+# How? tbd
+
+def basis(state,action):
+	basis = []
+	return basis
--- basis.py
+++
@@ -1,6 +0,0 @@
-#-----Compute basis
-# input: sampled (state, action) pair
-# output: phi (column vector of length equal to total number of basis functions, each entry is corresponding basis function evaluated at the (s,a) pair)
-# Definition: Computes the basis functions for given (state,action) pair
-#
-# How? tbd
