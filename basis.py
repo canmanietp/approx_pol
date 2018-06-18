d918347622ff3e35a2f71230b69389a4e35a491b
Canmanie <canmanie@iMac-werk.local>
2018-06-18 15:16:18 +0200

    basis fix

--- basis.py
+++ basis.py
@@ -1,7 +1,7 @@
 #-----Compute basis
 # input: sampled (state, action) pair
 # output: phi (column vector of length equal to total number of basis functions, each entry is corresponding basis function evaluated at the (s,a) pair)
-# Definition: Computes the basis functions for given (state,action) pairs
+# Definition: Computes the basis functions for given (state,action) pair(s)
 #
 # How? tbd
 
